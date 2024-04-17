12ae7b637f8f8134d4714f9ce43e6421a3f6c881
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-04-17 16:29:18 +0200

    try now

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,11 @@
--- prepares a MySQL server for the AirBnB clone
--- create a database
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- create a user and grant privileges
+-- A script that prepare a MySql server for the project
+
+-- Creating User
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
+
+-- Creating DataBase
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Granting Privileges
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
