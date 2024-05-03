b2aabe2295c0803a86214af8a7ef68b2a5c3ad18
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-05-03 18:50:14 +0300

    try task 2 final please

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,14 +4,16 @@
 from fabric.api import local, env, run, put
 from datetime import datetime
 import os
+from fabric.exceptions import CommandExecutionError
+
 env.hosts = ["54.234.73.177", "18.233.62.214"]
 env.user = 'ubuntu'
 
 
 def do_pack():
-    """pack all content within web_static
-    into a .tgz archive
-    The archive will be put in versions/
+    """Pack all content within web_static into a .tgz archive.
+
+    The archive will be put in versions/.
     """
     if not os.path.exists("versions"):
         local("mkdir versions")
@@ -26,33 +28,32 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """deploy package to remote server
+    """Deploy package to remote server.
+
     Arguments:
-        archive_path: path to archive to deploy
+        archive_path: Path to archive to deploy.
     """
     if not archive_path or not os.path.exists(archive_path):
         return False
     put(archive_path, '/tmp')
-    ar_name = archive_path[archive_path.find("/") + 1: -4]
+    ar_name = os.path.basename(archive_path)[:-4]
     try:
         run('mkdir -p /data/web_static/releases/{}/'.format(ar_name))
         run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'.format(
                 ar_name, ar_name
         ))
         run('rm /tmp/{}.tgz'.format(ar_name))
-        run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'.format(
-                ar_name, ar_name
-        ))
+        run('mv /data/web_static/releases/{}/web_static/* '
+            '/data/web_static/releases/{}/'.format(
+                ar_name, ar_name))
         run('rm -rf /data/web_static/releases/{}/web_static'.format(
             ar_name
         ))
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ \
-            /data/web_static/current'.format(
-            ar_name
-        ))
+        run('ln -s /data/web_static/releases/{}/ '
+            '/data/web_static/current'.format(ar_name))
         print("New version deployed!")
         return True
-    except:
+    except CommandExecutionError as e:
+        print("Deployment failed:", str(e))
         return False
